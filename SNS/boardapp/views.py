6d770c41646480dbae2146fb6ffe259a3959bb66
yoshi <yoshix47@gmail.com>
2020-06-02 20:02:40 +0900

    commit

---
+++ SNS/boardapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def signupfunc(request):
+    user2 = User.objects.get(username='yoshi')
+    print(user2.email)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+        return render(request, 'signup.html', {'some': 100})
+    return render(request, 'signup.html', {'some':100})
